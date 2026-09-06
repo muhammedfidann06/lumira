9851d7de87fc4e2392ac695a7e399b17f7c43110
Muhammed Fidan <muhammedfidann.06@icloud.com>
2026-09-06 11:52:56 +0300

    Add files via upload

--- pwa.js
+++ pwa.js
@@ -2107,7 +2107,7 @@ window.PWA = {
     });
     return { onLine: navigator.onLine, badgeVisible: !!(document.getElementById('pwa-offline') || {}).classList && document.getElementById('pwa-offline').classList.contains('in') };
   },
-  version: '1.7.41',
+  version: '1.7.42',
   isStandalone: function () { return isStandalone; }
 };
 
